9067f9dc20574902c6cd49d9223be3044724e0ee
Denis Laca <denis.laca@exponea.com>
2019-03-30 13:02:36 +0100

    adjust scripts

--- src/app/peer.js
+++ src/app/peer.js
@@ -12,5 +12,11 @@ const server = createServer(app);
 const peerserver = ExpressPeerServer(server, options);
 
 app.use('/api', peerserver);
+peerserver.on('connection', function(id) { 
+    console.log("connected ", id)
+});
 
+peerserver.on('disconnect', function(id) { 
+    console.log("disconnect ", id)
+});
 server.listen(8080);
