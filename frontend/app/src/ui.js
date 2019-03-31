b7457eca973134c41d2ff1e432b9bad88c966916
Denis Laca <denis.laca@exponea.com>
2019-03-31 03:14:02 +0200

    daco neviem uz co

--- frontend/app/src/ui.js
+++ frontend/app/src/ui.js
@@ -1,20 +1,20 @@
 import videoJs from 'video.js'
 import net from 'net'
 const createUI = (sender, reciever) => {
-  navigator.getUserMedia(
-    {video: true, audio: true},
-    (localMediaStream) => {
-      const video = document.querySelector('video')
-      video.src = window.URL.createObjectURL(localMediaStream)
-      video.onloadedmetadata = (e) => {
-        const player = videoJs('my-player')
-        player.play()
-      }
-    },
-    (e) => {
-      console.error('Error getting user media', e)
-    }
-  )
+  // navigator.getUserMedia(
+  //   {video: true, audio: true},
+  //   (localMediaStream) => {
+  //     const video = document.querySelector('video')
+  //     video.src = window.URL.createObjectURL(localMediaStream)
+  //     video.onloadedmetadata = (e) => {
+  //       const player = videoJs('my-player')
+  //       player.play()
+  //     }
+  //   },
+  //   (e) => {
+  //     console.error('Error getting user media', e)
+  //   }
+  // )
 
   reciever.on('connection', (conn) => {
     conn.on('data', (data) => {
@@ -24,24 +24,23 @@ const createUI = (sender, reciever) => {
   let connection
   const send = document.querySelector('#send')
   send.addEventListener('click', () => {
-    connection.send('afoj')
-    connection.send('prefix')
+    connection && connection.send('afoj')
+    connection && connection.send('prefix')
   })
   const connect = document.querySelector('#connect')
+  const server = net.createServer((socket) => {
+
+    socket.write('Echo server\r\n')
+    socket.on('data', (data) => {
+      connection && connection.send(data)
+    })
+  })
+  server.listen(1337, '127.0.0.1')
+
   connect.addEventListener('click', () => {
     connection = sender.connect(`${process.env.CONNECT_TO}_reciever`)
     connection.on('open', () => {
-      connection.send('posielam data')
-      const server = net.createServer((socket) => {
-
-        socket.write('Echo server\r\n')
-        socket.on('data', (data) => {
-          console.log(data)
-          connection.send(data)
-        })
-      })
-
-      server.listen(1337, '127.0.0.1')
+      connection && connection.send('posielam data')
     })
 
   })
