76f421607a56fd3e1f32300872769f2e9529c559
Siegrift <e.tesarr@gmail.com>
2019-03-31 11:01:49 +0200

    WIP

--- frontend/app/src/index.js
+++ frontend/app/src/index.js
@@ -1,50 +1,11 @@
 import createUI from './ui'
-import createControllers from './controllers'
-import Stats from 'stats.js'
 import Peer from 'peerjs'
-import envProperties from 'dotenv'
 
-// apply properties from .env file
-envProperties.config()
-
-// in development show FPS counter
-if (process.env.NODE_ENV === 'development') {
-  const stats = new Stats()
-  stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
-  document.body.appendChild(stats.dom)
-
-  window.requestAnimationFrame(function loop() {
-    stats.update()
-    window.requestAnimationFrame(loop)
-  })
-}
-
-// const sender = new Peer(`${process.env.MY_CONNECTION}_sender_video`, {
-//   host: '10.236.255.137',
-//   port: 8080,
-//   path: '/api',
-// })
-// const reciever = new Peer(`${process.env.MY_CONNECTION}_reciever_video`, {
-//   host: '10.236.255.137',
-//   port: 8080,
-//   path: '/api',
-// })
-
-// const voiceSender = new Peer(`${process.env.MY_CONNECTION}_sender_voice`, {
-//   host: '10.236.255.137',
-//   port: 8080,
-//   path: '/api',
-// })
-
-// const voiceReciever = new Peer(`${process.env.MY_CONNECTION}_reciever_voice`, {
-//   host: '10.236.255.137',
-//   port: 8080,
-//   path: '/api',
-// })
-var connectTo, sender, reciever, voiceSender, voiceReciever
-const prefixMaRukyNaopak = () => {
+let connectTo, sender, reciever, voiceSender, voiceReciever
+const makeConnection = () => {
+  console.log('Making connection')
   connectTo = document.getElementById('calleeID').value
-  console.log(sender, reciever, voiceSender, voiceReciever)
+
   createUI(sender, reciever, voiceSender, voiceReciever, connectTo)
   document.getElementById('formWrapper').style.opacity = 0
   setTimeout(() => {
@@ -52,9 +13,10 @@ const prefixMaRukyNaopak = () => {
   }, 500)
 }
 
+const setupConnection = () => {
+  console.log('Setting up connection')
+  document.getElementById('connect').removeEventListener('click', setupConnection)
 
-const defaultListener = () => {
-  document.getElementById('connect').removeEventListener('click', defaultListener)
   const myConnection = document.getElementById('callerID').value
   sender = new Peer(`${myConnection}_sender_video`, {
     host: '10.236.255.137',
@@ -86,17 +48,7 @@ const defaultListener = () => {
 
   document.getElementById('callersIDContainer').style.left = '-100%'
   document.getElementById('connect').innerHTML = '<i class="material-icons left">share</i> Connect'
-  document.getElementById('connect').addEventListener('click', prefixMaRukyNaopak)
+  document.getElementById('connect').addEventListener('click', makeConnection)
 }
 
-document.getElementById('connect').addEventListener('click', defaultListener)
-
-try {
-} catch (err) {
-  // eslint-disable-next-line
-  console.error('Connection failed', err)
-  document.getElementById('loading').classList.toggle('visible')
-  document.getElementById('error').classList.toggle('visible')
-  const elem = document.getElementById('error')
-  elem.innerHTML = 'Connection failed, please try later!'
-}
+document.getElementById('connect').addEventListener('click', setupConnection)
